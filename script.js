dea593fa2678a995faf723e5cf9bdda2907a51f8
daiki <67625825+k1tikurisu@users.noreply.github.com>
2025-01-02 19:05:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@
 window.onload = function () {
   const body = document.querySelector('main')
   body.style.visibility = 'visible'
+
+  const currentYear = new Date().getFullYear()
+  const copyrightElement = document.querySelector('.copy')
+  if (copyrightElement) {
+    copyrightElement.innerHTML = `&copy; ${currentYear} daiki`
+  }
 }
